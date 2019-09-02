a30f5ec9924552ad8d1f6adcd4d41ba2b09ba560
RobinMarchal <robinmarchal02@gmail.com>
2019-09-02 11:31:36 +0200

    Modif JS

--- js/main.js
+++ js/main.js
@@ -1,72 +1,5 @@
 (function($) {
 
-	// scroll
-	// var scrollWindow = function() {
-	// 	$(window).scroll(function(){
-	// 		var $w = $(this),
-	// 				st = $w.scrollTop(),
-	// 				navbar = $('.ftco_navbar'),
-	// 				sd = $('.js-scroll-wrap');
-
-	// 		if (st > 150) {
-	// 			if ( !navbar.hasClass('scrolled') ) {
-	// 				navbar.addClass('scrolled');	
-	// 			}
-	// 		} 
-	// 		if (st < 150) {
-	// 			if ( navbar.hasClass('scrolled') ) {
-	// 				navbar.removeClass('scrolled sleep');
-	// 			}
-	// 		} 
-	// 		if ( st > 350 ) {
-	// 			if ( !navbar.hasClass('awake') ) {
-	// 				navbar.addClass('awake');	
-	// 			}
-				
-	// 			if(sd.length > 0) {
-	// 				sd.addClass('sleep');
-	// 			}
-	// 		}
-	// 		if ( st < 350 ) {
-	// 			if ( navbar.hasClass('awake') ) {
-	// 				navbar.removeClass('awake');
-	// 				navbar.addClass('sleep');
-	// 			}
-	// 			if(sd.length > 0) {
-	// 				sd.removeClass('sleep');
-	// 			}
-	// 		}
-	// 	});
-	// };
-	// scrollWindow();
-
-	
-	// var counter = function() {
-		
-	// 	$('#section-counter').waypoint( function( direction ) {
-
-	// 		if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-	// 			var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-	// 			$('.number').each(function(){
-	// 				var $this = $(this),
-	// 					num = $this.data('number');
-	// 					console.log(num);
-	// 				$this.animateNumber(
-	// 				  {
-	// 				    number: num,
-	// 				    numberStep: comma_separator_number_step
-	// 				  }, 7000
-	// 				);
-	// 			});
-				
-	// 		}
-
-	// 	} , { offset: '95%' } );
-
-	// }
-	// counter();
-
 	var contentWayPoint = function() {
 		var i = 0;
 		$('.ftco-animate').waypoint( function( direction ) {
@@ -102,33 +35,6 @@
 		} , { offset: '90%' } );
 	};
 	contentWayPoint();
-	$('.partners-car').owlCarousel({
-        items: 4,
-        margin: 30,
-        loop:true,
-	    autoplay:true,
-	    autoplayTimeout:2500,
-	    autoplayHoverPause:true,
-        dots: false,
-        responsiveClass:true,
-        responsive : {
-        	320  : {
-        		items: 1
-		    },         
-		   	360  : {
-        		items: 1
-		    }, 
-		    480  : {
-        		items: 2
-		    },
-		   	768  : {
-        		items: 3
-		    },
-            1024 : {
-            	items : 4
-            }
-		}
-	 });
 
 	 $('.container a[href^="#"]').on('click', function(evt){
 		var target = $(this).attr('href');
